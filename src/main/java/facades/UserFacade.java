4afc0a0483362103a5397cdddeb4826a89175c27
KD131 <cph-kd131@cphbusiness.dk>
2021-11-26 15:15:26 +0100

    adds basic signup

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -1,5 +1,6 @@
 package facades;
 
+import entities.Role;
 import entities.User;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -40,6 +41,17 @@ public class UserFacade {
         } finally {
             em.close();
         }
+        return user;
+    }
+
+    public User signup(String username, String password) {
+        EntityManager em = emf.createEntityManager();
+        User user = new User(username, password);
+        Role role = new Role("user");
+        user.addRole(role);
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
         return user;
     }
 
--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -32,6 +32,7 @@ public class ApplicationConfig extends Application {
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
+        resources.add(security.SignupEndpoint.class);
         resources.add(security.errorhandling.AuthenticationExceptionMapper.class);
         resources.add(security.errorhandling.NotAuthorizedExceptionMapper.class);
     }
