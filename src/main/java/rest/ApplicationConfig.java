a4c8cf36ffb0573f6226c706bf82d64288e9c1ef
KD131 <71063280+KD131@users.noreply.github.com>
2021-12-02 12:53:09 +0100

    moves get author endpoint to author resource (/api/author/{id})

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.API_ExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.AuthorResource.class);
         resources.add(rest.DemoResource.class);
         resources.add(rest.LibraryResource.class);
         resources.add(rest.RenameMeResource.class);
